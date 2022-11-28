f794a9aa9ec5ff214276ed380b92f092229977a8
Ulises <javierulisesmtz@gmail.com>
2022-11-27 21:07:53 -0600

    Initial Commit

---
+++ casoPractico/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  casoPractico
+//
+//  Created by Javier Ulises Martinez Sanchez on 27/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
